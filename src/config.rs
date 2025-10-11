c84b1f5b75ebf7c6baa83c1777cd431ef905c648
kloikjj <ng2fsxcb2r@privaterelay.appleid.com>
2025-10-11 18:40:35 +0800

    config.rs

--- src/config.rs
+++ src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["rd.617101.xyz"];
+pub const RS_PUB_KEY: &str = "ralX8gz3MlcJpglFWqcH7DzXFWsQhww8bM1KJ+ZtTBY=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
