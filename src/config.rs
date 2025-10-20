7582d2cfcc1eefaa25090d0dbcbff410d9de3dd8
Shenliehuozhi <5424462+shenliehuozhi@users.noreply.github.com>
2025-10-20 20:26:18 +0800

    modify server parameter

--- src/config.rs
+++ src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["119.23.152.22"];
+pub const RS_PUB_KEY: &str = "gRBRxOBGTwiNg3JlhYecb2FcR+1Somp2G6mFfDZVkXs=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
