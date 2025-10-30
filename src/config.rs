34932f1d869ab27b79a8d8eb820752b1d9341ea3
duhuilai <18513930063@163.com>
2025-10-30 21:13:20 +0800

    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -105,8 +105,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["222.128.49.178"];
+pub const RS_PUB_KEY: &str = "vkX6TxJqhU1WFC6qoptiakHikDZUNCiC15UQWS4pexI=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
