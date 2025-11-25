b11aaf2977b7b7e0c5ac96d272f843ac5b50149c
camxl <camxl@163.com>
2025-11-25 09:24:20 +0800

    修改 config.rs

--- src/config.rs
+++ src/config.rs
@@ -106,8 +106,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["camxl.3322.org"];
+pub const RS_PUB_KEY: &str = "gtac8Kzcq5OhM5mqlbjnBIHnNLyW9vuZ5s3vQfd4Wl4=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
