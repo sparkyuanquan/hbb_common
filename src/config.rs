d7ee5626d165dba7a07ccfc358899a508496dc47
葉桜 <1904536409@qq.com>
2025-11-27 23:24:21 +0800

    改为自己的ID/中继服务器

--- src/config.rs
+++ src/config.rs
@@ -106,8 +106,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["42.194.169.148"];
+pub const RS_PUB_KEY: &str = "lfFjiTyhrMggOx76Wlv7hWANheTHuQ17fIimbu1t11A=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
