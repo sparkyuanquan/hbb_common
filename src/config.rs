bbba3a0150db6b53169b9168d42ec8533e07e983
lidaye88 <52451587+lidaye88@users.noreply.github.com>
2025-12-30 15:48:04 +0800

    config.rs

--- src/config.rs
+++ src/config.rs
@@ -106,8 +106,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["117.172.29.53"];
+pub const RS_PUB_KEY: &str = "k3IYtZ0gqwegXMF62GavOMW+OKJHJt5xP49bs3bIOc8=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
