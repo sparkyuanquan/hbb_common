60ee7cdf3f7037e9dbace7863dbcc5b66381e84a
shiyu-zhu-code <145332066+shiyu-zhu-code@users.noreply.github.com>
2026-01-16 14:57:03 +0800

    1

--- src/config.rs
+++ src/config.rs
@@ -106,8 +106,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["112.124.30.162"];
+pub const RS_PUB_KEY: &str = "AxckR02iM1m+cRUKwepv7kPiQ33rUqWEnCkx6vw4FXc=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
